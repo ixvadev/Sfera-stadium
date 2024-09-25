cdd91f4045451b7583f81c553fbc0ee19cec23b9
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-09-25 18:20:44 +0500

    active

--- src/components/help.tsx
+++ src/components/help.tsx
@@ -2,7 +2,7 @@ function Help() {
   return (
     <>
         <div className="w-[20%] bg-[#D1D3D4] h-screen">
-            <div className="text-[20px] py-[15px] hover:bg-[#FFFFFF]">
+            <div className="active text-[20px] py-[15px] hover:bg-[#FFFFFF]">
                 <i className="fa-solid fa-terminal px-4 "></i>
                 <a href="">Dashboart</a>
             </div>
@@ -12,7 +12,7 @@ function Help() {
                 <a href="">Masters</a>
             </div>
 
-            <div className="text-[20px] py-[15px] hover:bg-[#FFFFFF]" >
+            <div class  Name="text-[20px] py-[15px] hover:bg-[#FFFFFF]" >
                 <i className="fa-solid fa-user-group px-4"></i>
                 <a href="">Clients</a>
             </div>
