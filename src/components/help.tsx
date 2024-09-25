4bb392a292156df2ffbf45e0472aebbb13a3492f
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-09-25 22:20:32 +0500

    Linklarni hammasi ulandi

--- src/components/help.tsx
+++ src/components/help.tsx
@@ -1,21 +1,28 @@
+import { Link } from 'react-router-dom';
 function Help() {
   return (
     <>
         <div className="w-[20%] bg-[#f1f3f5] h-screen">
-            <div className="text-[20px] py-[15px] hover:bg-[#dcdcdc]">
-                <i className="fa-solid fa-terminal px-4 "></i>
-                <a href="">Dashboart</a>
-            </div>
+            <Link to={"/admin"}>
+                <div className="text-[20px] py-[15px] hover:bg-[#dcdcdc]">
+                    <i className="fa-solid fa-terminal px-4 "></i>
+                    <a href="">Dashboart</a>
+                </div>
+            </Link>
 
-            <div className="text-[20px] py-[15px] hover:bg-[#dcdcdc]">
-                <i className="fa-solid fa-user-tie px-5"></i>
-                <a href="">Masters</a>
-            </div>
-
-            <div className="text-[20px] py-[15px] hover:bg-[#dcdcdc]" >
-                <i className="fa-solid fa-user-group px-4"></i>
-                <a href="">Clients</a>
-            </div>
+            <Link to={"/master"}>
+                <div className="text-[20px] py-[15px] hover:bg-[#dcdcdc]">
+                    <i className="fa-solid fa-user-tie px-5"></i>
+                    <a href="">Masters</a>
+                </div>
+            </Link>
+            
+            <Link to={"/client"}>
+                <div className="text-[20px] py-[15px] hover:bg-[#dcdcdc]" >
+                    <i className="fa-solid fa-user-group px-4"></i>
+                    <a href="">Clients</a>
+                </div>
+            </Link>
         </div>
     </>
   )
