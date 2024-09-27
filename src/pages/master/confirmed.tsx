4834e310562b2abf99f93447d961719059c9d989
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-09-27 16:50:25 +0500

    eror edint

--- src/pages/master/confirmed.tsx
+++ src/pages/master/confirmed.tsx
@@ -79,7 +79,7 @@ const Master: React.FC = () => {
                             </div>
                             <div className="flex justify-between items-center mt-4">
                                 <button className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800">Previous</button>
-                                <span>1/19</span>
+                                <span>1/1</span>
                                 <button className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800">Next</button>
                             </div>
                         </div>
